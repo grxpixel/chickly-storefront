4c667369ce263e571a858b3a38cb4bb8395c27fe
Anand kushwaha <anandkushwaha09967@gmail.com>
2025-07-27 02:58:04 +0530

    logo update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -70,7 +70,7 @@ export function Header({
 
   {/* Center: Logo */}
   <NavLink prefetch="intent" to="/" className="text-2xl tracking-normal">
-    <img src="/app/assets/logo1.png" alt="Logo" className="inline-block w-[100px] h-auto" />
+    <img src="https://cdn.shopify.com/s/files/1/0661/0495/1028/files/DEB_4420.jpg?v=1751279595" alt="Logo" className="inline-block w-[100px] h-auto" />
   </NavLink>
 
   {/* Right: CTAs */}
@@ -96,7 +96,7 @@ export function Header({
               isScrolled ? 'text-xl sm:text-2xl' : 'text-2xl sm:text[28px]'
             }`}
           >
-            <img src="/app/assets/logo1.png" alt="Logo" className="inline-block w-[150px] h-auto ml-2" />
+            <img src="https://cdn.shopify.com/s/files/1/0661/0495/1028/files/DEB_4420.jpg?v=1751279595" alt="Logo" className="inline-block w-[150px] h-auto ml-2" />
           </NavLink>
           {/* desktop navigation  */}
           <div className="hidden lg:flex flex-1-px-12 ">
